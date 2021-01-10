88c3187aead771f95406ad737f86d43c8b34e15b
Avinash Pancham <avinashpancham@outlook.com>
2021-01-10 22:24:06 +0100

    Update backend

--- src/backend/main.py
+++ src/backend/main.py
@@ -2,5 +2,6 @@ import logging
 from tweepy_utils import start_stream
 
 if __name__ == "__main__":
-    logging.warning("Backend started")
+    logging.basicConfig(level=logging.INFO)
+    logging.info("Backend started")
     start_stream()
