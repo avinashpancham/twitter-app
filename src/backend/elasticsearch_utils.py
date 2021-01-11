b80700b5c820ba8e18195ce67e5e9eff09dbd645
Avinash Pancham <avinashpancham@outlook.com>
2021-01-11 23:21:34 +0100

    Add NER logic

--- src/backend/elasticsearch_utils.py
+++ src/backend/elasticsearch_utils.py
@@ -1,12 +1,40 @@
 import logging
 from datetime import datetime
+from typing import List
 
+import spacy
+from langdetect import detect
 from _typing import JsonType
 from elasticsearch import Elasticsearch
 
+from stopwords import dutch_english_stop_words
 
-def store_data(data: JsonType) -> None:
+ALLOWABLE_SPACY_LABELS = ("ORG", "LOC", "EVENT", "NORP", "FAC", "GPE", "PERSON")
+
+
+def process_and_store_data(data: JsonType) -> None:
+    topics = extract_topics(data["text"])
+    store_data(data, topics)
+
+
+def extract_topics(text: str) -> List[str]:
+    # Twitter language info often not available
+    dutch = detect(text) == "nl"
+    nlp_model = spacy.load("nl_core_news_sm") if dutch else spacy.load("en_core_web_sm")
+
+    doc = nlp_model(text.replace("#", ""))
+    return [
+        ent.text.lower()
+        for ent in doc.ents
+        if ent.label_ in ALLOWABLE_SPACY_LABELS
+        and ent.text.lower() not in dutch_english_stop_words
+        and ent.text.isalnum()
+    ]
+
+
+def store_data(data: JsonType, topics: List[str]) -> None:
     es = Elasticsearch([{"host": "elasticsearch-1-vm", "port": 9200}])
+
     es_data = {
         "id": data["id"],
         "created_at": datetime.strptime(
@@ -21,6 +49,7 @@ def store_data(data: JsonType) -> None:
         "location_name": data["place"]["name"]
         if data["place"]
         else data["user"]["location"].split(",")[0].strip(),
+        "topics": topics,
     }
 
     es.index(index="tweets", body=es_data)
@@ -45,6 +74,7 @@ def local_testing() -> None:
                 "text": "Test message",
                 "user_id": 1,
                 "user_name": "Test",
+                "topics": ["Test"],
             },
         )
 
